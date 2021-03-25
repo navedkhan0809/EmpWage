374ea566ad12de36ddea979b4f1e5e2427bb9aed
Naved Khan <navedkhan0809@gmail.com>
2021-03-25 12:06:09 +0530

    Added: Employee wages for a month using for-loop

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,22 +2,27 @@
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRatePerHr=20
-empCheck=$((RANDOM%3))
+numWorkingDays=20
 
-case $empCheck in
-
-$isFullTime)
-      empHrs=8
-      ;;
-
-$isPartTime)
-      empHrs=4
-      ;;
-*)
-      empHrs=0
-      ;;
-esac
-
+for (( day=1; day<=$numWorkingDays; day++))
+do
+   empCheck=$((RANDOM%3))
+      case $empCheck in
+         $isFullTime)
+            empHrs=8
+            ;;
+         $isPartTime)
+            empHrs=4
+            ;;
+         *)
+            empHrs=0
+            ;;
+      esac
 salary=$(($empHrs*$empRatePerHr))
-echo $salary
+totalSalary=$(($totalSalary + $salary))
+
+done
+
+echo "Total salary is $totalSalary"
