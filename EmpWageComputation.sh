d2a8598d47a077099a46ddb25b823aab78fc4238
Naved Khan <navedkhan0809@gmail.com>
2021-03-25 12:20:55 +0530

    Added: Employee wages using Dictionary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -11,6 +11,8 @@ numWorkingDays=20
 totalEmpHrs=0
 totalWorkingDays=0
 
+declare -A dailyWage
+
 function getWorkingHrs() {
       case $1 in
          $isFullTime)
@@ -32,9 +34,10 @@ do
    empCheck=$((RANDOM%3))
    empHrs="$( getWorkingHrs $empCheck )"
    totalEmpHrs=$(($totalEmpHrs + $empHrs))
-   dailyWage[$totalWorkingDays]=$(($empHrs*$empRatePerHr))
+   dailyWage["Day "$totalWorkingDays]=$(($empHrs*$empRatePerHr))
 done
 
 totalSalary=$(($totalEmpHrs*$empRatePerHr))
 echo "Total salary is $totalSalary"
 echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
