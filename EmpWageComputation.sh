a04edab4c6d6ed576665b70edd21dbe635e70da8
Naved Khan <navedkhan0809@gmail.com>
2021-03-25 11:27:58 +0530

    Added: Employee Daily Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 
+empRatePerHr=20
+empHrs=8
 empCheck=$((RANDOM%2))
 isPresent=1
 
 if [ $empCheck -eq $isPresent ]
 then
-   echo "Employee is Present"
+   salary=$(($empHrs*$empRatePerHr))
 else
-   echo "Employee is absent"
+   salary=0
 fi
+
+echo $salary
